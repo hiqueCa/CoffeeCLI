44c59fc098fb3cdb1cd21a5014abab0d1c2d781e
Henrique Caltram <henrivai@hotmail.com>
2023-12-22 16:36:21 -0300

    Implement skeleton for CoffeeCli::CLI#describe_brew method

--- coffee_cli/lib/coffee_cli/cli.rb
+++ coffee_cli/lib/coffee_cli/cli.rb
@@ -4,5 +4,20 @@ require 'thor'
 
 module CoffeeCli
   class CLI < Thor
+    desc "describe_brew", "describe your latest coffee brewing experience"
+
+    option :coffee_name, required: true
+    option :coffee_grams, required: true
+    option :water_mls, required: true
+
+    def describe_brew
+      brew_info = {
+        name: options[:coffee_name],
+        grams: options[:coffee_grams],
+        mililiters: options[:water_mls]
+      }
+
+      CoffeeCli::RegisterBrew.call(brew_info)
+    end
   end
 end
